a1f77bd08a99439035160caa5703d8fd181fed91
Ashar Farooq <ashar744@gmail.com>
2020-07-29 00:50:36 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+var request = new XMLHttpRequest();
+
+var input = window.prompt("What meme topic do you want to see?");
+
+//var input1 = alert(input))
+request.open('GET', "https://api.giphy.com/v1/gifs/random?api_key=X1eRcC1LAcPrE0DP7luuizxZt5JW1Z3j&tag=" +input+  "&rating=g");
+
+request.onload = function() {
+	var response = request.response;
+	var parsedData = JSON.parse(response);
+	var originalUrl = parsedData.data.images.original.url;
+	console.log(originalUrl);
+
+	//Create gif on page
+	var gif = document.createElement('img');
+	gif.setAttribute('src', originalUrl);
+	document.body.appendChild(gif);
+};
+
+request.send();

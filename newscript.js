523c4742852b35bc13cb17dc740aed2635c18939
Ashar Farooq <ashar744@gmail.com>
2020-07-29 00:59:39 -0400

    Create newscript.js

---
+++ newscript.js
@@ -0,0 +1,18 @@
+var request = new XMLHttpRequest();
+
+//var input1 = alert(input))
+request.open('GET', "https://api.giphy.com/v1/gifs/random?api_key=X1eRcC1LAcPrE0DP7luuizxZt5JW1Z3j&tag=college&rating=g");
+
+request.onload = function() {
+	var response = request.response;
+	var parsedData = JSON.parse(response);
+	var originalUrl = parsedData.data.images.original.url;
+	console.log(originalUrl);
+
+	//Create gif on page
+	var gif = document.createElement('img');
+	gif.setAttribute('src', originalUrl);
+	document.body.appendChild(gif);
+};
+
+request.send();
